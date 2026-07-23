12339a887c83d7f7734a62127bdf8b899ffad562
ArmandoRojas <armando.rojas04@inacapmail.cl>
2026-07-24 00:01:35 +0200

    FormularioMascota+Event

--- mi-app/src/components/MascotaForm.jsx
+++ mi-app/src/components/MascotaForm.jsx
@@ -1,8 +1,28 @@
 function MascotaForm(){
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("hice click")
+    }
+
     return(
         <>
-    
+                <h3>Formulario Mascotas</h3>
+            <form onSubmit={e => handleSubmit(e)}>
+                <label>Nombre<input type="text"/></label>
+                <label>Descripcion<input type="text"/></label>
+                <label>Imagen<input type="text"/></label>
+                <label>Estado<input type="text"/></label>
+                <label>TipodeAnimal<input type="text"/></label>
+                <label>Edad<input type="text"/></label>
+                <label>Raza<input type="text"/></label>
+                <label>Sexo<input type="text"/></label>
+                <label>Tamaño<input type="text"/></label>
+                <button>Agregar</button>
+                <p></p>
+
+            </form>
+        
         </>
 
     )
