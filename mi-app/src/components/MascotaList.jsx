06fb4e3c9be2bd402e0f92f6a4ac563cee7ccd6a
Matias Lillo <matias.lillo15@inacapmail.cl>
2026-07-23 23:29:42 -0400

    feat: listar mascota funcionando

--- mi-app/src/components/MascotaList.jsx
+++ mi-app/src/components/MascotaList.jsx
@@ -1,1 +1,29 @@
+function MascotaList({lista}) {
+    return (
+        <>
+            <h3>Lista Mascotas</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Descripcion</td>
+                        <td>Imagen</td>
+                        <td>Estado</td>
+                        <td>Tipo de animal</td>
+                        <td>Edad</td>
+                        <td>Raza</td>
+                        <td>Sexo</td>
+                        <td>Tamaño</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        lista.map(m => <tr key={m.id}><td>{m.nombre}</td><td>{m.descripcion}</td><td><img src={m.imagen} alt={m.nombre} width="80" /></td><td>{m.estado}</td><td>{m.tipo_animal}</td><td>{m.edad}</td><td>{m.raza}</td><td>{m.sexo}</td><td>{m.tamano}</td></tr>)
+                    }
+                </tbody>
+            </table>
+        </>
+    )
+}
 
+export default MascotaList;
