408af2947f325156a95ac8d159ff40fac2c87b1c
ArmandoRojas <armando.rojas04@inacapmail.cl>
2026-07-26 23:42:33 +0200

    implemento de boton elimiar

--- mi-app/src/components/MascotaList.jsx
+++ mi-app/src/components/MascotaList.jsx
@@ -23,7 +23,7 @@ function MascotaList({lista, onAdd}) {
                 </thead>
                 <tbody>
                     {
-                        lista.map(m => <tr key={m.id}><td>{m.nombre}</td><td>{m.descripcion}</td><td><img src={m.imagen} alt={m.nombre} width="80" /></td><td>{m.estado}</td><td>{m.tipo_animal}</td><td>{m.edad}</td><td>{m.raza}</td><td>{m.sexo}</td><td>{m.tamano}</td></tr>)
+                        lista.map(m => <tr key={m.id}><td>{m.nombre}</td><td>{m.descripcion}</td><td><img src={m.imagen} alt={m.nombre} width="80" /></td><td>{m.estado}</td><td>{m.tipo_animal}</td><td>{m.edad}</td><td>{m.raza}</td><td>{m.sexo}</td><td>{m.tamano}</td><td><button>Eliminar</button></td></tr>)
                     }
                 </tbody>
             </table>
